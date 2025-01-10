3e351d03020b4912cbf1538d09d1c1457f2f14ef
Mikhail <kozyrev.mikhail@russianpost.ru>
2025-01-10 10:38:12 +0300

    - drop detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "2.0.20"
     id("org.jetbrains.intellij.platform") version "2.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
-    id("io.gitlab.arturbosch.detekt") version "1.23.7"
-}
-
-detekt {
-    toolVersion = "1.23.7"
-    config.setFrom("$projectDir/config/detekt.yml")
-    buildUponDefaultConfig = true
 }
 
 group = "com.kebastos.kulibin"
